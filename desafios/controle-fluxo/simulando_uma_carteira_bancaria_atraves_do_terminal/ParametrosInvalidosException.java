441fbe833eeff189282382e2b2dcdea739a6976e
Alessandro <Alessandro1395271@outlook.com>
2024-07-16 12:47:00 -0300

    Resolução do desafio, criação de parametros, validações/invalidações

---
+++ desafios/controle-fluxo/simulando_uma_carteira_bancaria_atraves_do_terminal/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception {
+    // Construtor que recebe uma mensagem e a passa para a classe Exception
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
